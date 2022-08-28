44b9405fa403e5f01e8b903fc3042f9aee2f9c95
wingram1 <waingram96@gmail.com>
2022-08-28 16:21:57 -0400

    city search form and map click functionality'

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
+
 import Map from "../Components/Map";
+import CitySearch from "../Subcomponents/CitySearch";
 
 export function Home() {
+
+  const [locationSelect, setLocationSelect] = useState([39.0997, -94.5786])
+
   return (
     <div>
       <h1>Hello Drip Planner!</h1>
-      <Map />
+      <CitySearch />
+      <Map locationSelect={locationSelect}/>
     </div>
   );
 }
---
+++ src/Subcomponents/CitySearch.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+
+export default function CitySearch() {
+  // state of search input
+  const [inputState, setInputState] = useState("");
+
+  // handles search submit
+  const handleCitySearch = (e: React.FormEvent<HTMLInputElement>) => {};
+
+  return (
+    <form
+      id="citySearchForm"
+      onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(`inputState: ${inputState}`);
+      }}
+    >
+      <input
+        id="citySearch"
+        name="citySearch"
+        placeholder="Search for a City"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setInputState(e.target.value);
+          console.log(e.target.value);
+        }}
+        onSubmit={handleCitySearch}
+      ></input>
+      <button type="submit" form="citySearchForm">
+        Go!
+      </button>
+    </form>
+  );
+}
