23b7595b48545bfad29f86a8a2a143601e5ea822
wingram1 <waingram96@gmail.com>
2022-09-04 15:53:37 -0400

    line graph now renders properly

--- src/Components/Map/Subcomponents/LocaleChart.tsx
+++ src/Components/Map/Subcomponents/LocaleChart.tsx
@@ -1,71 +1,82 @@
-import Chart, { ChartItem } from "chart.js/auto";
-import { useRef, useEffect, useState } from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { useMemo } from "react";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 // Component renders inside WeatherDetails
-
 export const LocaleChart = (props: any) => {
   const { weatherData } = props;
 
-  const labels = weatherData.map((day: any) => {
-    return new Date(day.dt * 1000)
-      .toLocaleDateString("en-US", {
-        weekday: "short",
-        month: "numeric",
-        day: "numeric",
-      })
-      .replace(",", "");
-  });
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "8-day Forecast",
+      },
+    },
+  };
 
-  const chartData = {
-    labels: labels,
-    datasets: weatherData.map((day: any, i: number) => {
-      return {
-        label: labels[i],
+  const chartData: any = useMemo(() => {
+    return {
+      labels: weatherData.map((day: any) => {
+        return new Date(day.dt * 1000)
+          .toLocaleDateString("en-US", {
+            weekday: "short",
+            month: "numeric",
+            day: "numeric",
+          })
+          .replace(",", "");
+      }),
+      datasets: [{
+        label: "Temperature",
         backgroundColor: "rgb(255, 99, 132)",
         borderColor: "rgb(255, 99, 132)",
-        data: [day.temp.max, day.temp.min],
-      };
-    }),
-  };
-
-  const chartRef = useRef<Chart | null>(null);
-
-  const canvasCallback = (canvas: HTMLCanvasElement | null) => {
-    if (!canvas) return;
-    const ctx = canvas.getContext("2d");
-    if (ctx) {
-      chartRef.current = new Chart(ctx, {
-        type: "line",
-        data: chartData,
-        options: {},
-      });
-    }
-  };
-
-  // update chart when props are updated
-  useEffect(() => {
-    // must verify chart exists
-    if (chartRef.current) {
-      chartRef.current.data = chartData;
-      chartRef.current.update();
-    }
-
-    // cleanup function
-    return () => {
-      chartRef.current?.destroy();
+        data: weatherData.map((day: any, i: number) => {
+          return day.temp.max;
+        }),
+      }],
     };
-  }, [chartData]);
+  }, [weatherData]);
 
-  return (
-    <canvas
-      id="localeChart"
-      width="100%"
-      height="200px"
-      aria-label="Local Weather Details"
-      role="img"
-      ref={canvasCallback}
-    ></canvas>
-  );
+  console.log(chartData);
+
+  //   const labels = [1, 2, 3, 4, 5, 6, 7];
+  //   const data = {
+  //     labels: labels,
+  //     datasets: [
+  //       {
+  //         label: "My First Dataset",
+  //         data: [65, 59, 80, 81, 56, 55, 40],
+  //         fill: false,
+  //         borderColor: "rgb(75, 192, 192)",
+  //         tension: 0.1,
+  //       },
+  //     ],
+  //   };
+
+  return <Line options={options} data={chartData} />;
 };
 
 export default LocaleChart;
