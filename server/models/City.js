14e06248254aead495971b4a773db88645bd33ba
wingram1 <waingram96@gmail.com>
2022-10-26 21:53:12 -0400

    schema for City model

--- server/models/City.js
+++ server/models/City.js
@@ -1,2 +1,35 @@
-const { Schema, model, Types } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const CitySchema = new Schema({
+  city: {
+    type: String,
+    required: "There must be a city name.",
+    trim: true,
+  },
+  state: {
+    type: String,
+    required: "There must be a state name.",
+    trim: true,
+  },
+  lat: {
+    type: Number,
+    required: "Latitude coordinate required.",
+  },
+  lon: {
+    type: Number,
+    required: "Longitude coordinate required.",
+  },
+  daily: {
+    type: Array,
+    required: "48-hour forecast required.",
+  },
+  hourly: {
+    type: Array,
+    required: "8-day forecast required.",
+  },
+});
+
+const City = model("City", CitySchema);
+
+module.exports = City;
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(require("./routes"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network-api",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/drip-planner",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
